2c86ed6ca0bd73fb28141359f00b87f4a73170f0
Tom Barto <tom.barto@outlook.com>
2017-02-10 17:08:11 -0500

    initial commit

--- app.js
+++ app.js
@@ -7,8 +7,6 @@
 var express = require('express');
 var http = require('http');
 
-var socket = require('./socket/handleConnections.js');
-
 var app = express();
 var server = http.createServer(app);
 
@@ -17,7 +15,6 @@ var port = process.env.PORT || 8080;
 /* Configuration */
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-//app.set('port', 3000);
 
 app.get('*',function (req, res) {
       res.redirect('/');
@@ -27,21 +24,8 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
-/* Socket.io Communication */
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', socket);
-io.sockets.on('call',function(data){
-  console.log('received call');
-  socket.broadcast.emit('call',data);
-});
-io.sockets.on('clearCall', function(data){
-  console.log('clearing call');
-  socket.broadcast.emit('clearCall', data);
-});
-
 /* Start server */
 server.listen(port, function (){
-  //console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
   console.log('Express server listening on port %d in %s mode', port, app.get('env'));
 });
 
